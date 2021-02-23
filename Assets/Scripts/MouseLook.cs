332e7a924572cc1e05faee459fb66dfe6812ac2a
hawkcrash <zoroark40@gmail.com>
2021-02-23 13:57:00 +0100

    InputSystem
    Added Input System
    Added test Scene named Untitled
    Input not 100% functional

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MouseLook : MonoBehaviour
+{
+        
+    private float _cameraPitch = 0.0f;
+            
+    [SerializeField] private Transform playerCamera = null;
+    [SerializeField] private float mouseSensitivity = 3.5f;
+    [SerializeField] [Range(0.0f, 0.5f)] private float mouseSmoothTime = 0.03f;
+    [SerializeField] private bool lockCursor = true;
+
+    private Vector2 _lookVector;
+    private Vector2 _currentMouseDelta = Vector2.zero;
+    private Vector2 _currentMouseDeltaVelocity = Vector2.zero;
+    private void Start()
+    {
+        if (!lockCursor) return;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void OnLook(InputValue value) => _lookVector = value.Get<Vector2>();
+
+    private void Update()
+    {
+        UpdateMouseLook();
+        if (Keyboard.current.escapeKey.wasPressedThisFrame)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            //EditorApplication.isPlaying = false;
+        }
+    }
+
+    private void UpdateMouseLook()
+    {
+        _currentMouseDelta = Vector2.SmoothDamp(_currentMouseDelta, _lookVector, ref _currentMouseDeltaVelocity,
+            mouseSmoothTime);
+            
+        _cameraPitch -= _currentMouseDelta.y * mouseSensitivity;
+
+        _cameraPitch = Mathf.Clamp(_cameraPitch, min:-90, max:90);
+
+        playerCamera.localEulerAngles = Vector3.right * _cameraPitch;
+
+        transform.Rotate(eulers: Vector3.up * _currentMouseDelta.x * mouseSensitivity);
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 6f;
+    [SerializeField] private float gravity = -13f;
+    [SerializeField] [Range(0.0f, 0.5f)] private float moveSmoothTime = 0.3f;
+        
+
+    private float _velocityY = 0.0f;
+    private Vector2 _moveDirection;
+    private Vector2 _currentDirection = Vector2.zero;
+    private Vector2 _currentDirectionVelocity = Vector2.zero;
+        
+    private CharacterController _controller = null;
+        
+    private void Start() => _controller = GetComponent<CharacterController>();
+
+    private void OnMove(InputValue value)
+    {
+        _moveDirection = value.Get<Vector2>();
+    }
+
+    private void Update()
+    {
+        UpdateMovement();
+    }
+
+    private void UpdateMovement()
+    {
+        _currentDirection = Vector2.SmoothDamp(_currentDirection, _moveDirection,
+            ref _currentDirectionVelocity, moveSmoothTime);
+            
+        if (_controller.isGrounded)
+        {
+            _velocityY = -2f;
+        }
+
+        _velocityY += gravity * Time.deltaTime;
+            
+        var velocity = (transform.forward * _currentDirection.y + transform.right * _currentDirection.x) * moveSpeed + Vector3.up * _velocityY;
+        _controller.Move(velocity * Time.deltaTime);
+    }
+}
