e86348de190748a7d6b851b5da06a03665755a35
Evennm <72070250+Evennm@users.noreply.github.com>
2021-03-02 13:25:06 +0100

    Voicelines Added

---
+++ Assets/Scripts/GameEnder.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
+
+public class GameEnder : MonoBehaviour
+{
+    private Animator animator;
+
+    [SerializeField] private int WaitTime = 20;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    
+    
+     public IEnumerator RollCredits()
+    {
+        yield return new WaitForSeconds(WaitTime);
+        animator.SetBool("CreditsOut", true);
+    }
+     
+    
+   public IEnumerator Finish()
+    {
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene("StartMenu");
+    }
+}
